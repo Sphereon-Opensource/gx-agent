c20cf365cc51b0096058703e3dae019312199b0b
Maikel Maas <mmaas@sphereon.com>
2023-01-11 15:53:28 +0100

    FMA-7 Enhance the Gaia-X compliance service to enable verification of Verifiable Presentations
       - Implemented the ecosystem actions;

--- packages/gx-compliance-client-cli/src/ecosystem.ts
+++ packages/gx-compliance-client-cli/src/ecosystem.ts
@@ -1,15 +1,13 @@
-import { program } from 'commander'
+import {InvalidArgumentError, program} from 'commander'
 import { getAgent } from './setup'
 import { GxEntityType } from './types'
 import { printTable } from 'console-table-printer'
-import fs from "fs";
 
 const ecosystem = program.command('ecosystem').description('gx-participant ecosystem')
 
 ecosystem
   .command('add')
-  //todo: description???
-  .description('creates a new gaia-x compliance client')
+  .description('Adds a new Gaia-x ecosystem to the client')
   .requiredOption('-n, --name <string>', 'ecosystem name')
   .requiredOption('-url, --ecosystem-url <string>', 'gaia-x ecosystem server address')
   .action(async (cmd) => {
@@ -37,11 +35,34 @@ ecosystem
 .option('-e, --ecosystem <string>', 'alias of your ecosystem')
 .action(async (cmd) => {
   try {
-    const sd = JSON.parse(fs.readFileSync(cmd['sd-file'], 'utf-8'))
-    console.log(sd)
-    // const agent = getAgent(program.opts().config)
-    // const selfDescription = await agent.acquireComplianceCredentialFromUnsignedParticipant()
-    // printTable([{...selfDescription}])
+    const agent = getAgent(program.opts().config)
+    const participantVChash = cmd['sd-id']
+    const complianceVChash = cmd['compliance-id']
+
+    //FIXME it makes more sense to move this to GaiaxComplianceClient since we need to call additional agent methods to retrieve key/did info
+
+    // Beginning
+    const sd = await agent.dataStoreGetVerifiableCredential({
+      hash: participantVChash
+    })
+    const did = sd.credentialSubject.id as string
+    const didResolutionResult = await agent.resolveDid({ didUrl: did })
+    if (!didResolutionResult.didDocument?.verificationMethod) {
+      throw new InvalidArgumentError("There is no verification method")
+    }
+    const verificationMethodId = didResolutionResult.didDocument.verificationMethod[0].id as string
+    const keyRef = (await agent.didManagerGet({ did })).keys[0].kid
+    //End
+
+    const selfDescription = await agent.onboardParticipantWithCredentialIds({
+      verificationMethodId,
+      selfDescribedVcHash: participantVChash,
+      complianceCredentialHash: complianceVChash,
+      purpose: sd.proof.purpose,
+      challenge: sd.proof.challenge,
+      keyRef
+    })
+    printTable([{...selfDescription}])
   } catch(e:unknown) {
     console.error(e)
   }
